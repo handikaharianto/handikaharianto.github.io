1be946e00692bfbc430c8b1133d452e2eebe5457
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2022-03-21 07:34:31 +0800

    Fix project section

--- js/displayProjectList.js
+++ js/displayProjectList.js
@@ -18,7 +18,9 @@ function displayProjectList() {
             </div>
             <div class="project__content">
               <h4 class="project__name">
-                <a href=${website} target="_blank">${name}</a>
+                <a href=${
+                  website ? website : github
+                } target="_blank">${name}</a>
               </h4>
               <p class="project__text">
                 ${text}
